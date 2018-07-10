9dfce20f52ca8c763e2ceaf211325ed5d2d7f214
Kathy N <kathyvng@gmail.com>
2018-07-10 16:31:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,12 +16,10 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper] #returnaddition
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for] #returnaddition
 end
 
 def dennis_ritchies_language
@@ -55,6 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+	programmer_hash[:dennis_ritchie][:languages] #returnaddition
 end
 
 def adding_matz
