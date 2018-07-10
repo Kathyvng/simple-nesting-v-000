94064e787970361cb22131925f00b2baf656c163
Kathy N <kathyvng@gmail.com>
2018-07-10 16:33:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto] = {
     known_for: "Ruby",
     languages: ["LISP, C"]
-  }  
+  }
   programmer_hash
 	programmer_hash =
  		{
@@ -126,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
